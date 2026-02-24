13ea9a6ea902d80b7b52c45c197bb9c5746e4790
Subramanya Y A <subramanyaya@MY9D4M7QKM.local>
2026-02-24 17:47:18 +0530

    small fix

--- run_supervised.py
+++ run_supervised.py
@@ -14,13 +14,13 @@ from reward_generator import RewardCalculator
 def run(config):
     
     path = config.path
-    orders = pd.read_csv(f'{path}/orders.csv')
-    order_skus = pd.read_csv(f'{path}/order_skus.csv')
+    orders = pd.read_parquet(f'{path}/orders.parquet')
+    order_skus = pd.read_parquet(f'{path}/order_skus.parquet')
     sku_vol = pd.read_parquet(f'{path}/sku_vol.parquet')
 
     df = order_skus.merge(orders,on=['GROUP_ID','ORDER_ID'])
     df = df.merge(sku_vol, on='SKU_ID',how='left')
-    
+
     df['ORDER_DATE'] = pd.to_datetime(df['ORDER_DATE'],format='mixed')
     df['IS_fast_delivery'] = df['DELIVERY_TYPE'].map({'fast_delivery':1,'slow_delivery':0})
 
